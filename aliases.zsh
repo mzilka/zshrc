1c1b6e20597a037528ecd5c96a5c146384ca7d1f
Martin Zilka <mzilka35@gmail.com>
2017-10-21 12:31:10 +0200

    changed dindkey to vi

--- aliases.zsh
+++ aliases.zsh
@@ -7,7 +7,7 @@
 export EDITOR="vim"
 # But still use emacs-style zsh bindings
 # http://superuser.com/questions/403355/how-do-i-get-searching-through-my-command-history-working-with-tmux-and-zshell
-bindkey -e
+bindkey -v
 
 alias vi="vim"
 alias aedit=" $EDITOR $ZSH_CONFIG/aliases.zsh; source $ZSH_CONFIG/aliases.zsh"
